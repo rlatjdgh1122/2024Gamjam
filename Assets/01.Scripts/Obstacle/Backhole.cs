44834affcd15171cdfdbc649169272079c9acf39
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2024-01-02 10:50:24 +0900

    블랙홀 구현

---
+++ Assets/01.Scripts/Obstacle/Backhole.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class Backhole : MonoBehaviour
+{
+    [Header("플레이어 영향 줄 스탯")]
+    public int size = 30; //범위 크기 
+    public int strength = 20; //힘
+
+    [SerializeField] private Collider[] colls;
+    private void FixedUpdate()
+    {
+        colls = Physics.OverlapSphere(transform.position, size);
+        if (colls.Length > 0)
+        {
+            foreach (Collider col in colls)
+            {
+
+                var rb = col.GetComponent<Rigidbody>(); //렉걸리면 여기 수정할거임
+                if (rb != null)
+                {
+                    var obj = col.gameObject;
+                    var objPos = obj.transform.position;
+                    var dir = (transform.position - objPos).normalized;
+                    var dis = dir.magnitude;
+
+                    rb.AddForce(dir * dis);
+                }
+            }
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        //삭제시킴
+    }
+}
