2aa4be6cd4acab5d67f049d6641a58cfa2fd267c
leewnstj <ij35492581@gmail.com>
2024-01-03 00:56:31 +0900

    인트로씬 코드 작성 완료잉

--- Assets/01.Scripts/UI/Story/LoadStory.cs
+++ Assets/01.Scripts/UI/Story/LoadStory.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 [Serializable]
 public class Story
 {
     [TextArea] public string StoryContent;
+    public float Delay;
     public Sprite Img;
 }
 
@@ -19,11 +21,41 @@ public class LoadStory : MonoBehaviour
 
     public List<Story> storyList = new();
 
-    private void Update()
+    private string text;
+
+    private void Start()
     {
-        for(int i = 0; i < storyList.Count; ++i)
+        StartCoroutine(TextOuput());
+    }
+
+    private IEnumerator TextOuput()
+    {
+        for (int i = 0; i < storyList.Count; ++i)
         {
             _curSprite.sprite = storyList[i].Img;
+            _curTex.text = storyList[i].StoryContent;
+            text = _curTex.text;
+            _curTex.text = " ";
+
+            StartCoroutine(textPrint(storyList[i].Delay));
+
+            yield return new WaitForSeconds(storyList[i].StoryContent.Length * 0.125f + 2f);
+        }
+    }
+
+    IEnumerator textPrint(float delay)
+    {
+        int count = 0;
+
+        while (count != text.Length)
+        {
+            if (count < text.Length)
+            {
+                _curTex.text += text[count].ToString();
+                count++;
+            }
+
+            yield return new WaitForSeconds(delay);
         }
     }
 }
