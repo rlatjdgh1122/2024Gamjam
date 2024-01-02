415a67e61281b3b2b6c0df5fbb6c65189708d627
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2024-01-02 12:50:20 +0900

    운석 에셋 추가

---
+++ Assets/01.Scripts/Obstacle/Satellite.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Satellite : Obstacle
+{
+    protected override void Start()
+    {
+        var rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+
+        speed = MaxSpeed; //랜덤으로 스피드
+        dir = Vector3.forward;
+    }
+    protected override void Update()
+    {
+        transform.Translate(dir * speed * Time.deltaTime);
+    }
+    public override void CollisonEvent(Collision player)
+    {
+        throw new System.NotImplementedException();
+    }
+}
