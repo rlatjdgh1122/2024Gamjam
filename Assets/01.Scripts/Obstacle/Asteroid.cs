b8288083cd7a4ae8bde451d51fa22f631793333c
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2024-01-03 01:07:07 +0900

    속도조절 내가 할게

--- Assets/01.Scripts/Obstacle/Asteroid.cs
+++ Assets/01.Scripts/Obstacle/Asteroid.cs
@@ -12,7 +12,15 @@ public class Asteroid : SpawnObstacle
     }
     public override void CollisonEvent(Collision player)
     {
-        //플레이어의 온도, 속도를 줄여주고 크기를 줄여줌
+
+        if (player.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            //플레이어의 온도, 속도를 줄여주고 크기를 줄여줌
+            if (player.gameObject.TryGetComponent<PlayerManager>(out var Compo))
+            {
+                //Compo.GetMoveToForward;
+            }
+        }
         //내 오브젝트는 죽고 부숴지는 파티클이 나와야함
         PoolManager.Instance.Push(this);
     }
