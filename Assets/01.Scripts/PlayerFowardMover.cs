d1c4a758faae383371ca1fdb7894af5cfb772b2d
Chwfi <98933635+chwfi@users.noreply.github.com>
2024-01-02 23:14:31 +0900

    UI
    UI

---
+++ Assets/01.Scripts/PlayerFowardMover.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFowardMover : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+
+    public bool IsEnable = true;
+
+    private void Update()
+    {
+        if (IsEnable)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * _moveSpeed);
+        }  
+    }
+}
