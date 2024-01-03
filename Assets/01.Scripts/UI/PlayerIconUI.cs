1cb5f0fba93b14099b8bb33f7b61cfe426b18ea2
Chwfi <98933635+chwfi@users.noreply.github.com>
2024-01-03 12:28:00 +0900

    현재 위치, 거리 UI 제작
    현재 위치, 거리 UI 제작

--- Assets/01.Scripts/UI/PlayerIconUI.cs
+++ Assets/01.Scripts/UI/PlayerIconUI.cs
@@ -8,7 +8,10 @@ public class PlayerIconUI : MonoBehaviour
 {
     [SerializeField] private float _rotateSpeed;
     [SerializeField] private float _moveSpeed;
-    [SerializeField] private RectTransform _target;
+
+    [SerializeField] private RectTransform _parentTrm;
+    [SerializeField] private RectTransform _startPos;
+    [SerializeField] private RectTransform _endPos;
 
     private Image _icon;
 
@@ -17,9 +20,14 @@ public class PlayerIconUI : MonoBehaviour
         _icon = GetComponent<Image>();
     }
 
+    private void Start()  
+    {
+        _parentTrm.anchoredPosition = _startPos.anchoredPosition;
+    }
+
     private void Update()
     {
         _icon.rectTransform.Rotate(Vector3.back * Time.deltaTime * _rotateSpeed);
-        _icon.rectTransform.parent.Translate(Vector3.right * Time.deltaTime * _moveSpeed);
+        _parentTrm.Translate(Vector3.right * Time.deltaTime * PlayerManager.Instance.GetMoveToForward.MoveSpeed / 2000);
     }
 }
